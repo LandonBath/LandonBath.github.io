f222da222d4765210dba40709445e1d1a8e9cfb2
LandonBath <bathlandon@gmail.com>
2025-03-26 23:21:48 +0000

    runtime

--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -20,19 +20,69 @@ var runLevels = function (window) {
     // BEGIN EDITING YOUR CODE HERE
     function createObstacles(x, y, hitSize, damage){
       var hitZoneSize = hitSize;// the size of hitbox for the obstacles and you
-    var damageFromObstacle = damage;// how much damage it does
-    var obstacleHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);// creates the obstacles
-    obstacleHitZone.x = x;// sets the x coordinates for the obstacles
-    obstacleHitZone.y = y;// sets the y coordinates for the obstacles
-    game.addGameItem(obstacleHitZone);// adds the obstacles to the game
-    var obstacleImage = draw.bitmap("img/sawblade.png");// draws a obstacle bitmap and store it as obst
-    obstacleHitZone.addChild(obstacleImage);
-    obstacleImage.x = -25;
-    obstacleImage.y = -25;
+      var damageFromObstacle = damage;// how much damage it does
+      var obstacleHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);// creates the obstacles
+      obstacleHitZone.x = x;// sets the x coordinates for the obstacles
+      obstacleHitZone.y = y;// sets the y coordinates for the obstacles
+      game.addGameItem(obstacleHitZone);// adds the obstacles to the game
+      var obstacleImage = draw.bitmap("img/sawblade.png");// draws a obstacle bitmap and store it as obstacles
+      obstacleHitZone.addChild(obstacleImage);// attach the image to the obstacle hitzone
+      obstacleImage.x = -25;// position the image on the hitzone's x value by moving it left 25 pixel
+      obstacleImage.y = -25;// position the image on the hitzone's y value by moving it up 25 pixel
+      obstacleHitZone.rotationalVelocity = 3;
     }
-    createObstacles( 400, groundY - 50, 25, 10);
-    createObstacles( 400, groundY - 50, 25, 20);
-    createObstacles( 400, groundY - 50, 25, 100);
+    createObstacles( 700, groundY - 50, 25, 10);
+    createObstacles( 500, groundY - 50, 25, 20);
+    createObstacles( 1100, groundY - 50, 25, 100);
+
+    function createEnemy(x, y, speed, health){
+      var enemy = game.createGameItem("enemy", 25);// creates enemy game item and adds it to game
+      var redSquare = draw.rect(50, 50, "red");// creates a red square and stores it in the ver redSquare
+      redSquare.x = -25;// offsets the image from the hitzone by -25 pixels
+      redSquare.y = -25;// offsets the image from the hitzone ny -25 pixels
+      enemy.addChild(redSquare);// add red square as a child to enemy code
+      enemy.x = x;// x pos of enemy
+      enemy.y = y;// y pos of enemy
+      game.addGameItem(enemy);// adds enemy to the game
+      enemy.velocityX -= speed;// how fast the enemy moves on the x axis
+      enemy.rotationalVelocity = 10;// sets the rotational velocity of the enemy
+      enemy.onPlayerCollision = function () {
+        game.changeIntegrity(health)// subtracts 10 health from hallBot's HUD
+      };
+      enemy.onProjectileCollision = function () {
+        game.increaseScore(100);// increases your score when halle shoots the enemy
+        enemy.fadeOut();// enemy fades out when Halle shoots them
+        //enemy.shrink();
+        //enemy.flyTo(0,0);
+      };
+    }
+    createEnemy(400, groundY - 50, 5, -15);
+    createEnemy(800, groundY - 50, 5, -15);
+    createEnemy(1600, groundY - 50, 5, -15);
+    
+
+    function createReward(x, y, speed, health){
+      var reward = game.createGameItem("reward", 25);// creates reward game item and adds it to game
+      var blueSquare = draw.rect(50, 50, "blue");// creates a blue square and stores it in the ver blueSquare
+      blueSquare.x = -25;// offsets the image from the hitzone by -25 pixels
+      blueSquare.y = -25;// offsets the image from the hitzone ny -25 pixels
+      reward.addChild(blueSquare);// add blue square as a child to reward code
+      reward.x = x;// x pos of reward
+      reward.y = y;// y pos of reward
+      game.addGameItem(reward);// adds reward to the game
+      reward.velocityX -= speed;// how fast the reward moves on the x axis
+      reward.onPlayerCollision = function () {
+        game.increaseScore(50);// increases your score when halle shoots the enemy
+        game.changeIntegrity(health)// subtracts 10 health from hallBot's HUD
+        reward.fadeOut();// reward fades out when Halle shoots them
+        //enemy.shrink();
+        //enemy.flyTo(0,0);
+      };
+    }
+
+    createReward(500, groundY - 100, 3, 10);
+
+
     function startLevel() {
     
       // TODO 13 goes below here
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -29,7 +29,8 @@ var background = function (window) {
         // ANIMATION VARIABLES HERE //////////////////////////////////////
         //////////////////////////////////////////////////////////////////
         // TODO (several):
-      
+        var tree;
+        var buildings = [];
       
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
@@ -50,25 +51,28 @@ var background = function (window) {
             moon.scaleY = 0.5;// scales the moon's height
             background.addChild(moon);// add the moon to the background container
             
-            var circle = draw.circle(10, "white", "LightGray", 2);// creates a circle
-            circle.x = canvasWidth * Math.random();// makes a circle at a random x position
-            circle.y = groundY * Math.random();// makes a circle at a random y position
-            background.addChild(circle);// makes the circle apart of the bachground
+            for(var i = 0; i < 100; i++){
+                var circle = draw.circle(10, "white", "LightGray", 2);// creates a circle
+                circle.x = canvasWidth * Math.random();// set random x pos within canvas width
+                circle.y = groundY * Math.random();// set random y pos within groundY range
+                background.addChild(circle);// adds the star to the background containr
+            }
 
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            for (var i = 0; i < 5; ++i) { // limits the amount of buildings you can have
-                var buildingHeight = 300;// the defalt height of the buildings
-                var building = draw.rect(75, buildingHeight, "LightGray", "Black", 1);// makes the shape of the building
+            for (var i = 0; i < 5; i++) { // limits the amount of buildings you can have
+                var buildingColors = ["red", "blue", "yellow", "orange","purple"];
+                var buildingHeight = 300 * Math.random();// the defalt height of the buildings
+                var building = draw.rect(75, buildingHeight, buildingColors[i], "Black", 1);// makes the shape of the building
                 building.x = 200 * i;// the width of the building
                 building.y = groundY - buildingHeight;// the height of the building
                 background.addChild(building);// puts the buildings in the background
                 buildings.push(building);
-              }
+            }
             
             // TODO 3: Part 1 - Add a tree
             tree = draw.bitmap("img/tree.png");// creates a bitmap object using the moon image and stores it in the variable
-            tree.x = 0;// the x position of the tree
-            tree.y = 0;// the y position of the tree
+            tree.x = canvasWidth - 300;// the x position of the tree
+            tree.y = groundY - 225;// the y position of the tree
             background.addChild(tree);// put the tree in the background
             
         } // end of render function - DO NOT DELETE
@@ -83,18 +87,21 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 3: Part 2 - Move the tree!
-            tree.x = tree.x + 1;
-
+            tree.x -= 3;// moves the tree to the to the left by subtracting 3 from its current x pos
             if (tree.x < -200) {
-             tree.x = canvasWidth;
+                tree.x = canvasWidth;
             }
             
+            
             // TODO 4: Part 2 - Parallax
-            for (var i = 0; i < myArray.length; i++) {
-                var eachElement = myArray[i];
-              
-                // code to do something with each element
-              }
+            for (var i = 0; i < buildings.length; i++) {
+                var building = buildings[i];// the individual index of the buildings array stored in a variable building
+                building.x -= 1;// subtracts 1 from the building's x pos; animate to the left
+                if (building.x < -100){// checks if the x pos of the building is less than -100
+                    building.x = canvasWidth;// if true, reset building's x to the right side of canvas
+                }
+
+            }
 
         } // end of update function - DO NOT DELETE
         
