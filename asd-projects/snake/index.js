823de34b6f2b94602b504e91802d10c605e5636e
LandonBath <bathlandon@gmail.com>
2026-02-10 17:29:00 +0000

    snake

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -132,6 +132,17 @@ function moveSnake() {
     HINT: The snake's head will need to move forward 1 square based on the value
     of snake.head.direction which may be one of "left", "right", "up", or "down"
   */
+  if (snake.head.direction === "left") {
+  snake.head.column = snake.head.column - 1;
+  } else if (snake.head.direction === "right") {
+    snake.head.column = snake.head.column + 1;
+  } else if (snake.head.direction === "up") {
+    snake.head.row = snake.head.row - 1;
+  } else if (snake.head.direction === "down") {
+    snake.head.row = snake.head.row + 1;
+  }
+
+  repositionSquare(snake.head);
 
 
 
@@ -140,8 +151,17 @@ function moveSnake() {
 
 // TODO 9: Create a new helper function
 
+ function moveBodyAToBodyB(bodyA, bodyB) {
+  bodyA.row = bodyB.row;
+  bodyA.column = bodyB.column;
+  bodyA.direction = bodyB.direction;
+ }
 
-
+console.log("Moving body A to body B...");
+setTimeout(() => {
+  moveBodyAToBodyB(snake.body[1], snake.head);
+  repositionSquare(snake.body[1]);
+}, 2_000);
 
 
 function hasHitWall() {
