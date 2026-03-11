6b155f5533c5ba08cecba89208673663e9ae53cf
LandonBath <bathlandon@gmail.com>
2026-03-11 00:01:49 +0000

    image

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -21,9 +21,6 @@ function resetAndRender() {
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
 
-  applyFilter(image, decreaseBlue);
-  applyFilter(image, increaseGreenByBlue);
-
 
   // do not change the below line of code
   render($("#display"), image);
@@ -34,57 +31,23 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2, 3 & 5: Create the applyFilter function here
-function applyFilter(image, filterFunction) {
-  for (let i = 0; i < image.length; i++) {
-    for (let j = 0; j < image[i].length; j++) {
-      const currentPixel = image[i][j];
-      filterFunction(currentPixel);
-    }
-  }
-}
+
 
 
 
 // TODO 9 Create the applyFilterNoBackground function
 function applyFilterNoBackground(image, filterFunction) {
-  const bgColor = image[0][0];
   
-  for (let i = 0; i < image.length; i++) {
-    for (let j = 0; j < image[i].length; j++) {
-      const currentPixel = image[i][j];
-      
-      // Only apply if the pixel is NOT the background color
-      if (currentPixel.r !== bgColor.r || currentPixel.g !== bgColor.g || currentPixel.b !== bgColor.b) {
-        filterFunction(currentPixel);
-      }
-    }
-  }
+  
 }
 
 // TODO 6: Create the keepInBounds function
-function keepInBounds(value) {
-  if (value < 0) {
-    return 0;
-  } else if (value > 255) {
-    return 255;
-  } else {
-    return value;
-  }
-}
+ 
 
 // TODO 4: Create reddify filter function
-function reddify(pixel) {
-  pixel.r = keepInBounds(pixel.r + 50);
-}
 
 
 // TODO 7 & 8: Create more filter functions
-function decreaseBlue(pixel) {
-  pixel.b = keepInBounds(pixel.b - 50);
-}
 
-function increaseGreenByBlue(pixel) {
-  pixel.g = keepInBounds(pixel.g + pixel.b);
-}
 
 // CHALLENGE code goes below here
